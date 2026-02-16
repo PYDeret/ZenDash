6ce8d80e055a8978761af35236109c8c57237b80
PY <pierreyves.deret@gmail.com>
2026-02-16 19:34:09 +0100

    Mise en place de la qualité via les packages

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,11 +3,25 @@
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
+    ->exclude('vendor')
+    ->exclude('public/bundles')
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
+        '@PER-CS' => true,
         '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'declare_strict_types' => true,
+        'strict_comparison' => true,
+        'strict_param' => true,
+        'modernize_types_casting' => true,
+        'no_superfluous_phpdoc_tags' => true,
+        'phpdoc_summary' => false,
+        'yoda_style' => false,
     ])
     ->setFinder($finder)
+    ->setRiskyAllowed(true)
 ;

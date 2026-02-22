c691bf534d337fab509184eb2591a5db41f6e7ae
PY <pierreyves.deret@gmail.com>
2026-02-22 20:08:37 +0100

    Ajout des trad en back

--- src/Controller/Auth/LogoutController.php
+++ src/Controller/Auth/LogoutController.php
@@ -11,6 +11,6 @@ class LogoutController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \LogicException('');
     }
 }
